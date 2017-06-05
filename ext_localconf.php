ac99b8031517f3678dc028755e54a267e44da945
klamparski <klamparski@gmail.com>
2017-06-05 15:21:44 +0200

    Cleanup - Remove code not used anymore

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,11 +8,6 @@ call_user_func(
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['connectToDB'][$extKey]
             = \SourceBroker\Translatr\Hooks\LocallangXMLOverride::class
             .'->initialize';
-
-//        if (!\TYPO3\CMS\Core\Utility\GeneralUtility::_GET('eID')) {
-//            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][$extKey] =
-//                'SourceBroker\Translatr\Hooks\TypoScriptTranslate->loadTranslations';
-//        }
     },
     $_EXTKEY
 );
