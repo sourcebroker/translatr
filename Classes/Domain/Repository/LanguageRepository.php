e4328f442b6f35445f6f0bea0e1b62c134d5b283
klamparski <klamparski@gmail.com>
2017-06-05 12:21:58 +0200

    Implement languages domain model and repository

---
+++ Classes/Domain/Repository/LanguageRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SourceBroker\Translatr\Domain\Repository;
+
+/**
+ * Class LanguageRepository
+ *
+ * @package SourceBroker\Translatr\Domain\Repository
+ */
+class LanguageRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+
+}
