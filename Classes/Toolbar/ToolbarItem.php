229e792bcb2768e9702cf1bbcf14cec0513428fd
Michał Świderski <michal.swiderski2@gmail.com>
2017-06-28 16:06:50 +0200

    remove access to clear language cache for admin users

--- Classes/Toolbar/ToolbarItem.php
+++ Classes/Toolbar/ToolbarItem.php
@@ -40,16 +40,14 @@ class ToolbarItem implements \TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHookInt
     public function manipulateCacheActions(&$cacheActions, &$optionValues) {
         $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
 
+        $cacheActions[] = array(
+            'id'    => self::$itemKey,
+            'title' => $this->getLanguageService()->sL('LLL:EXT:translatr/Resources/Private/Language/locallang.xlf:flushLanguageCache'),
+            'href'  => BackendUtility::getAjaxUrl('language_cache::flushCache'),
+            'icon' => $this->iconFactory->getIcon('actions-system-cache-clear-impact-medium', Icon::SIZE_SMALL)->render()
+        );
+        $optionValues[] = self::$itemKey;
 
-        if ($this->getBackendUser()->isAdmin()) {
-            $cacheActions[] = array(
-                'id'    => self::$itemKey,
-                'title' => $this->getLanguageService()->sL('LLL:EXT:translatr/Resources/Private/Language/locallang.xlf:flushLanguageCache'),
-                'href'  => BackendUtility::getAjaxUrl('language_cache::flushCache'),
-                'icon' => $this->iconFactory->getIcon('actions-system-cache-clear-impact-medium', Icon::SIZE_SMALL)->render()
-            );
-            $optionValues[] = self::$itemKey;
-        }
     }
 
     /**
