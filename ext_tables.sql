ed396250556be40df50c6dfc9cf4a7d054573355
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-07-12 16:18:10 +0200

    [BUGFIX] Add hidden field to database altough is not needed for app but can be problem with some external apps wrongly adding hidden to sql.

--- ext_tables.sql
+++ ext_tables.sql
@@ -17,6 +17,7 @@ CREATE TABLE tx_translatr_domain_model_label (
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
 	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
 	starttime int(11) unsigned DEFAULT '0' NOT NULL,
 	endtime int(11) unsigned DEFAULT '0' NOT NULL,
 
